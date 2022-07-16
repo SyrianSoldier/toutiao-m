a919a26f825ee0b6e3f64bf22a4194934fe6f12a
SyrianSoldier <9975545+syriansoldier@user.noreply.gitee.com>
2022-07-16 21:33:13 +0800

    11

---
+++ src/views/Home/component/ArticleList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>文章列表</div>
+</template>
+
+<script>
+export default {
+  props: {
+    // 频道id
+    id: {
+      type: [String, Number],
+      required: true
+    }
+  }
+}
+</script>
+
+<style></style>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,9 +1,130 @@
 <template>
-  <div>首页</div>
+  <div>
+    <!-- 头部导航 -->
+    <van-nav-bar class="navbar">
+      <template #title>
+        <van-button round> <van-icon name="search" />搜索 </van-button>
+      </template>
+    </van-nav-bar>
+
+    <!-- tabs选项卡 -->
+    <van-tabs v-model="active" swipeable>
+      <van-tab v-for="item in myChannels" :key="item.id" :title="item.name">
+        <!-- 文章列表 -->
+        <ArticleList :id="item.id"></ArticleList>
+      </van-tab>
+
+      <!-- 更多的按钮 -->
+      <span class="toutiao toutiao-gengduo"></span>
+    </van-tabs>
+  </div>
 </template>
 
 <script>
-export default {}
+// 引入API
+import { getMyChannels } from '@/api'
+// 引入组件
+import ArticleList from './component/ArticleList.vue'
+export default {
+  name: 'Home',
+  components: {
+    ArticleList
+  },
+  data () {
+    return {
+      active: 2,
+      myChannels: []
+    }
+  },
+  created () {
+    // 获取我的频道列表
+    this.getMyChannels()
+  },
+  methods: {
+    // 获取我的频道列表
+    async getMyChannels () {
+      // res一定有data
+      try {
+        const { data } = await getMyChannels()
+        this.myChannels = data.data.channels
+      } catch (error) {
+        this.$toast.fail('请重新获取频道列表')
+      }
+    }
+  }
+}
 </script>
 
-<style></style>
+<style scoped lang="less">
+// 头部导航
+.navbar {
+  background-color: #3296fa;
+  color: #fff;
+  .van-button {
+    width: 555px;
+    height: 64px;
+    background-color: #5babfb;
+  }
+  :deep(.van-nav-bar__title) {
+    max-width: unset;
+  }
+  .van-button__text {
+    color: #fff;
+  }
+  .van-icon-search {
+    color: #fff;
+  }
+  .van-button--default {
+    border: 0.02667rem solid #5babfb;
+  }
+}
+//tabs选项卡
+:deep(.van-tabs__wrap) {
+  padding-right: 66px;
+
+  .van-tabs__nav {
+    padding-left: 0;
+    padding-right: 0;
+
+    /* tab标签 */
+    .van-tab {
+      border: 1px solid #eee;
+      width: 200px;
+      height: 82px;
+    }
+
+    /* tab标签下划线 */
+    .van-tabs__line {
+      width: 31px;
+      height: 6px;
+      background-color: #3296fa;
+      bottom: 40px;
+    }
+  }
+}
+
+/* 字体图标 */
+.toutiao-gengduo {
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 66px;
+  height: 82px;
+  font-size: 40px;
+  line-height: 82px;
+  text-align: center;
+  opacity: 0.6;
+  border-bottom: 1px solid #eee;
+
+  &::after {
+    content: '';
+    position: absolute;
+    left: 0;
+    top: 50%;
+    transform: translateY(-50%);
+    height: 70%;
+    width: 1px;
+    background-image: url('~@/assets/images/gradient-gray-line.png');
+  }
+}
+</style>
